fccbd335e76d6e8700305f2a8c6ef0b5437c44db
RobertoPacho <robertsonam1993@gmail.com>
2021-01-07 02:25:20 -0500

    commit inicial

---
+++ src/ec/ups/edu/metodos/Logica.java
@@ -0,0 +1,45 @@
+package ec.ups.edu.metodos;
+
+public class Logica {
+	private int id;
+	private String nombre;
+	private String apellido;
+	
+	
+	public Logica() {
+
+	}
+	
+	public Logica(int id, String nombre, String apellido) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String metodIntegranteUno() {
+		return "NarcisaAraujo";
+	}
+	public String metodIntegranteDos() {
+		return "RobertoPacho";
+	}
+
+}
---
+++ src/ec/ups/edu/metodos/Principal.java
@@ -0,0 +1,15 @@
+package ec.ups.edu.metodos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logica logica=new Logica();
+		
+		
+		logica.metodIntegranteUno();
+		logica.metodIntegranteDos();
+
+	}
+
+}
