52006403420fbe4f80bacf305e4a280ca5da7d79
narcisaaraujo <56529535+narcisaaraujo@users.noreply.github.com>
2021-01-07 04:05:08 -0500

    elimino metodo 2

--- src/ec/ups/edu/metodos/Principal.java
+++ src/ec/ups/edu/metodos/Principal.java
@@ -4,11 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Logica logica=new Logica();
-		
-		
-		Logica logica2=new Logica(2,"Roberto", "Pacho");
-		System.out.println(logica.metodIntegranteDos());
+		Logica logica=new Logica(1,"Narcisa","Araujo");
+		System.out.println(logica.metodIntegranteUno());
+				
+	
 
 	}
 
